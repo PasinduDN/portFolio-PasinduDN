56db3dbba81e23052928471fb806a9d2d791518c
Pasindu Dulanjana <pasindu@agrithmics.lk>
2024-07-16 01:14:16 +0530

    implement Week1

--- src/assets/Component/HowIWork/Intern/WeeklyTarget.jsx
+++ src/assets/Component/HowIWork/Intern/WeeklyTarget.jsx
@@ -6,42 +6,12 @@ function WeeklyTarget() {
     return (
         <div>
             <div className="accordion-body " style={{ margin: '10px 0 10px' }}>
-                {/* Main content of Accordion Item #1 */}
-                <div> Intro <code>.accordion-flush</code>  </div>
+                {/* Main content of Accordion Item #1 - <code>.accordion-flush</code>*/}
+                <div> Welcome to my weekly work recap!  </div>
                 <br />
                 {/* Nested accordions for Weeks */}
                 <div className="accordion" id="nestedAccordion">
-                    {/* content - 1.1 */}
-                    <div className="accordion-item">
-                        <h2 className="accordion-header">
-                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne12" aria-expanded="false" aria-controls="flush-collapseOne12">
-                                Week 01
-                            </button>
-                        </h2>
-                        <div id="flush-collapseOne12" className="accordion-collapse collapse" aria-labelledby="flush-headingOne12" data-bs-parent="#nestedAccordion">
-                            <div className="accordion-body ">
-                                {/* Content for Week 01 */}
-                                Placeholder content for Week 01. <br />
-                                <button className="btn" data-bs-toggle="modal" data-bs-target="#idOfWeek01"><code> See More</code> </button>
-                            </div>
-                            {/* <div className="btn btn-outline-info custom-button">See More</div> */}
-                        </div>
-                    </div>
-                    {/* content - 1.2 */}
-                    <div className="accordion-item">
-                        <h2 className="accordion-header">
-                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne13" aria-expanded="false" aria-controls="flush-collapseOne13">
-                                Week 02
-                            </button>
-                        </h2>
-                        <div id="flush-collapseOne13" className="accordion-collapse collapse" aria-labelledby="flush-headingOne13" data-bs-parent="#nestedAccordion">
-                            <div className="accordion-body btn">
-                                {/* Content for Week 02 */}
-                                Placeholder content for Week 02. <br />
-                                <button className="btn"><code> See More</code> <Week2></Week2></button>
-                            </div>
-                        </div>
-                    </div>
+
                     {/* content - 1.3 */}
                     <div className="accordion-item">
                         <h2 className="accordion-header">
@@ -50,13 +20,47 @@ function WeeklyTarget() {
                             </button>
                         </h2>
                         <div id="flush-collapseOne14" className="accordion-collapse collapse" aria-labelledby="flush-headingOne14" data-bs-parent="#nestedAccordion">
-                            <div className="accordion-body btn">
+                            <div className="accordion-body">
                                 {/* Content for Week 03 */}
                                 Placeholder content for Week 03. <br />
-                                <button className="btn"><code> See More</code> <Week3></Week3> </button>
+                                <button className="btn" data-bs-toggle="modal" data-bs-target="#idOfWeek01"><code> See More</code> </button>
                             </div>
                         </div>
                     </div>
+
+                    {/* content - 1.2 */}
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne13" aria-expanded="false" aria-controls="flush-collapseOne13">
+                                Week 02
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne13" className="accordion-collapse collapse" aria-labelledby="flush-headingOne13" data-bs-parent="#nestedAccordion">
+                            <div className="accordion-body">
+                                {/* Content for Week 02 */}
+                                Placeholder content for Week 02. <br />
+                                <button className="btn" data-bs-toggle="modal" data-bs-target="#idOfWeek01"><code> See More</code> </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* content - 1.1 */}
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne12" aria-expanded="false" aria-controls="flush-collapseOne12">
+                                <h6>Week 01</h6>
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne12" className="accordion-collapse collapse" aria-labelledby="flush-headingOne12" data-bs-parent="#nestedAccordion">
+                            <div className="accordion-body ">
+                                {/* Content for Week 01 */}
+                                This week marked my first experience in the office. 
+                                <button className="btn" data-bs-toggle="modal" data-bs-target="#idOfWeek01"><code> See More</code> </button>
+                            </div>
+                            {/* <div className="btn btn-outline-info custom-button">See More</div> */}
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
 
@@ -65,7 +69,6 @@ function WeeklyTarget() {
                 <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
@@ -73,7 +76,7 @@ function WeeklyTarget() {
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                            {/* <button type="button" class="btn btn-primary">Save changes</button> */}
                         </div>
                     </div>
                 </div>
--- src/assets/Component/HowIWork/Intern/Weeks/Week1.jsx
+++ src/assets/Component/HowIWork/Intern/Weeks/Week1.jsx
@@ -3,24 +3,30 @@ import customImg from '../../../../Images/Logo/Media.jpg'
 function Week1() {
     return (
         <div>
-            <h3>HTML-Ipsum</h3>
-            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p> <br />
-            <p>This is content of week1</p> <br />
-
+            <p>This week marked my first experience in the office, filled with learning opportunities and exciting beginnings. Here’s a breakdown of how my first week unfolded.</p> 
             <ul>
-                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                <li>Aliquam tincidunt mauris eu risus.</li>
-                <li>Vestibulum auctor dapibus neque.</li>
+                <li> <h6>Joining the Team : </h6> On my first day, I was warmly welcomed into the team. I was introduced to an Associate Software Engineer who would be my mentor. The project manager gave me an overview of the team and the project we are working on, setting the stage for an enriching learning experience.</li>
+                <li> <h6>Simple Student Management System: : </h6> During the initial days, my mentor assigned me a task to create a Simple Student Management System using CRUD operations. This task was pivotal in helping me understand the fundamentals of our project framework and how to carry out work efficiently. </li>
             </ul>
 
-            <ol>
-                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                <li>Aliquam tincidunt mauris eu risus.</li>
-                <li>Vestibulum auctor dapibus neque.</li>
-            </ol>
+            <p>Challenges and Solutions :</p> <br />
 
-            <p>This is content of week1</p> <br />
+            <ul>
+                <li> <h6> Learning Stored Procedures :</h6> After completing the student management system, my mentor introduced me to stored procedures. This was a new concept for me, and understanding how to implement them effectively posed a challenge. However, with my mentor's guidance, I was able to grasp the basics and apply them in our project. This experience highlighted the importance of database management and optimized data handling in software development. </li>
+            </ul>
 
+            <p>Learning and Development :</p> <br />
+
+            <ul>
+                <li> <h6> Understanding Project Architecture :</h6> One of the most valuable lessons this week was understanding the relationship between the controller, service, and model in our project. My mentor demonstrated how these components interact within our codebase, which was enlightening. I learned how to navigate through the project structure, and how each part plays a crucial role in the overall functionality. </li>
+            </ul>
+
+            <p>Upcoming Goals :</p> <br />
+
+            <ul>
+                <li> <h6> Deepening Technical Skills :</h6> Moving forward, I aim to deepen my understanding of stored procedures and their application. Additionally, I plan to explore more about the intricacies of the MVC architecture and how to leverage it for efficient project development. </li>
+                <li> <h6> Building Relationships: </h6> Another goal is to build stronger relationships within my team. Establishing good communication and collaboration is key to our collective success. </li>
+            </ul>
 
 
         </div>
